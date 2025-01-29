49c38f31b18ef0bae521a7f48f94a84ed3b51e10
Tearran <tearran@gmail.com>
2025-01-29 05:08:51 +0000

    fix array

--- tools/modules/software/module_openhab.sh
+++ tools/modules/software/module_openhab.sh
@@ -1,18 +1,19 @@
 module_options+=(
-	["module_octoprint,id"]="PRI0520"
-	["module_octoprint,maintainer"]="@igorpecovnik"
-	["module_octoprint,feature"]="module_octoprint"
-	["module_octoprint,desc"]="Install octoprint container"
-	["module_octoprint,example"]="install remove purge status help"
-	["module_octoprint,status"]="Active"
-	["module_octoprint,about"]=""
-	["module_octoprint,doc_link"]="https://transmissionbt.com/"
-	["module_octoprint,author"]="@armbian"
-	["module_octoprint,parent"]="software"
-	["module_octoprint,group"]="Printing"
-	["module_octoprint,port"]="7981"
-	["module_octoprint,arch"]="x86-64 arm64"
+	["module_openhab,id"]="PRI0520"
+	["module_openhab,maintainer"]="@igorpecovnik"
+	["module_openhab,feature"]="module_openhab"
+	["module_openhab,desc"]="Install octoprint container"
+	["module_openhab,example"]="install remove purge status help"
+	["module_openhab,status"]="Active"
+	["module_openhab,about"]=""
+	["module_openhab,doc_link"]="https://transmissionbt.com/"
+	["module_openhab,author"]="@armbian"
+	["module_openhab,parent"]="software"
+	["module_openhab,group"]="Printing"
+	["module_openhab,port"]="7981"
+	["module_openhab,arch"]="x86-64 arm64"
 )
+
 
 
 #
